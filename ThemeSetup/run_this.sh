ffa74bc41953ef02f21db7a509af93039e5e87db
Dhruvesh Surolia <dhruv8sh@outlook.com>
2021-10-15 12:09:22 +0530

    Update run_this.sh

--- ThemeSetup/run_this.sh
+++ ThemeSetup/run_this.sh
@@ -1,15 +1,5 @@
 dir=$(dirname $0)
 dir="$dir/FloatingDockPanelCustomLatte.layout.latte"
 echo $dir
-#!latte-dock --import-layout $dir
-#!latte-dock --replace
-qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
-    var allDesktops = desktops();
-    print (allDesktops);
-    for (i=0;i<allDesktops.length;i++)
-    {
-        d = allDesktops[i];
-        d.wallpaperPlugin = "org.kde.image";
-        d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
-        d.writeConfig("Image", "$(dirname $0)/Cyberpunk2077-wallpaper.jpg")
-    }'
+latte-dock --import-layout $dir
+latte-dock --replace
