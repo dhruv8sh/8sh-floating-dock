3be9c1e12b9f6a5451cb412d1c820b0356631d9d
Dhruvesh Surolia <dhruv8sh@outlook.com>
2021-10-15 12:13:26 +0530

    Update run_this.sh

--- AppsSetup/run_this.sh
+++ AppsSetup/run_this.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo pacman -Syu --noconfirm
 cd $(scriptdir)
-sudo pacman -S spotify yay bluej blender microsoft-edge-dev-bin intellij-idea-community yakuake tor-browser qbittorrent audacity steam-native-runtime gwenview latte elisa vlc --noconfirm
+sudo pacman -S spotify yay bluej blender microsoft-edge-dev-bin intellij-idea-community yakuake tor-browser qbittorrent audacity steam-native-runtime gwenview latte-dock elisa vlc --noconfirm
 yay -S spotify-adblock
