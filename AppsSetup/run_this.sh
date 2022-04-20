70fc3f69058be1214d0d125262c6d6c6265f9a02
Dhruvesh Surolia <dhruv8sh@outlook.com>
2022-04-20 19:03:50 +0530

    Delete AppsSetup directory

--- AppsSetup/run_this.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo pacman -Syu --noconfirm
-cd $(scriptdir)
-sudo pacman -S spotify yay bluej blender microsoft-edge-dev-bin intellij-idea-community yakuake tor-browser qbittorrent audacity steam-native-runtime gwenview latte-dock elisa vlc --noconfirm
-yay -S spotify-adblock
